880fe2039a6d257ed8b821c76e6ea6466ad054e5
Aleksandr Timokhin <adtimokhin@Aleksandrs-MacBook-Pro.local>
2024-02-06 17:43:22 -0600

    Updated number of vertical lines on smaller screens

--- app/layout.js
+++ app/layout.js
@@ -15,8 +15,8 @@ export default function RootLayout({ children }) {
         <link href="https://fonts.cdnfonts.com/css/helvetica-neue-55" rel="stylesheet"/>
       </Head>
       <body className="mx-[1.25rem] phone:mx-[1.25rem] tablet:mx-[2.5rem] laptop:mx-[5rem] pt-8 relative">
-        <div className="h-full w-[1px] bg-[#BBBBBB] absolute top-0 left-1/3 -translate-x-1 -z-10"></div>
-        <div className="h-full w-[1px] bg-[#BBBBBB] absolute top-0 right-1/3 translate-x-1 -z-10"></div>
+        <div className="h-full w-[1px] bg-[#BBBBBB] absolute tablet:inline hidden top-0 left-1/3 -translate-x-1 -z-10"></div>
+        <div className="h-full w-[1px] bg-[#BBBBBB] absolute top-0 tablet:right-1/3 right-1/2 translate-x-1 -z-10"></div>
         <NavBar/>
         {children}
         <Footer/>
